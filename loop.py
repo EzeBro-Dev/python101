8ca97b2b1a971491f7c4ca3915896eb000ee7ed7
Elue Ezekiel <elueezekiel2005@gmail.com>
2025-09-14 21:04:43 +0100

    Loop Added

---
+++ loop.py
@@ -0,0 +1,17 @@
+# FOR LOOP
+# for variable in sequence:
+    # do something with variable
+    
+
+for i in range(5):
+    print("Hello", i)
+
+
+# WHILE LOOP
+# while condition:
+    # do something
+
+count = 1
+while count <= 5:
+    print("count is:", count)
+    count += 1
